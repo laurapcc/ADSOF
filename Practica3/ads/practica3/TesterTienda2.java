39575d58e26187e6d4b0e677d620abf05eaec416
Laura de Paz <44983440+laurapcc@users.noreply.github.com>
2020-03-12 17:11:17 +0100

    Rename Practica3/TesterTienda2.java to Practica3/ads/practica3/TesterTienda2.java

--- Practica3/ads/practica3/TesterTienda2.java
+++ Practica3/ads/practica3/TesterTienda2.java
@@ -6,7 +6,7 @@ import java.util.List;
  * Segundo tester el tercer apartado de la P2
  * DEBE EJECUTARSE CON UN ARGUMENTO EN LA LINEA DE COMANDOS
  *
- * @param args[0] El primer y único argumento será el nombre del archivo de texto a leer
+ * @param args[0] El primer y Ãºnico argumento serÃ¡ el nombre del archivo de texto a leer
  * @author Profesores ADS
 
  */
