59fc354f7bc48e056265e7f16e70549bc0058c34
RubGarFue <RubGarFue@hotmail.com>
2020-04-27 00:59:33 +0200

    Apartado 2 COMMENTS

--- Practica5/ads/practica5/comparator/BlackBoxComparator.java
+++ Practica5/ads/practica5/comparator/BlackBoxComparator.java
@@ -16,6 +16,13 @@ import ads.practica5.graph.*;
 public class BlackBoxComparator<T, S> implements Comparator<ConstrainedGraph<T,S>> {
     private Map<Criteria, Set<Predicate<Node<T>>>> criteria = new HashMap<>();
 
+    /**
+     * Añade un criterio de comparacion al comparador de grafos
+     * 
+     * @param c criterio que rige al predicado de la comparacion
+     * @param p predicado que será utilizado por la función comparador
+     * @return mismo comparador con criterio añadido
+     */
     public BlackBoxComparator<T,S> addCriteria(Criteria c, Predicate<Node<T>> p) {
         if (criteria.containsKey(c)) {
             criteria.get(c).add(p);
@@ -28,6 +35,15 @@ public class BlackBoxComparator<T, S> implements Comparator<ConstrainedGraph<T,S
         return this;
     }
 
+    /**
+     * Compara dos grafos en base a los criterios que posee el propio
+     * comparador
+     * 
+     * @param o1 primer grafo sobre el que se realizará la comparación
+     * @param o2 segundo grafo sobre el que se realizará la comparación
+     * @return entero positvo si o1 es mayor que o2, negativo si o1 es menor
+     * y 0 si son iguales
+     */
     @Override
     public int compare(ConstrainedGraph<T, S> o1, ConstrainedGraph<T, S> o2) {
         int o1cumple = 0;
--- Practica5/ads/practica5/comparator/Criteria.java
+++ Practica5/ads/practica5/comparator/Criteria.java
@@ -15,6 +15,15 @@ import java.util.function.Predicate;
 public enum Criteria {
     EXISTENTIAL, UNITARY, UNIVERSAL;
 
+    /**
+     * Realiza un test sobre el grafo o en función del criterio y el predicado p
+     * 
+     * @param <T> tipo de dato genérico referenciando el tipo de nodos del grafo
+     * @param <S> tipo de dato genérico referenciando el tipo de edges del grafo
+     * @param o grafo sobre el que se realiza el test
+     * @param p predicado que utilizará el test
+     * @return true si el grafo ha pasado el test, false en caso contrario
+     */
     public <T,S> boolean test(ConstrainedGraph<T, S> o, Predicate<Node<T>> p) {
         if (equals(EXISTENTIAL))
             return o.exists(p);
